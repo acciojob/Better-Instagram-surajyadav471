1dc4344aa169c1e2ce8058f2a17c208031ed48fd
surajyadav471 <115660802+surajyadav471@users.noreply.github.com>
2023-03-04 14:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+const divs = document.querySelectorAll('.image');
+
+divs.forEach((div) => {
+  div.style.position = 'absolute';
+});
+
+let dragStartIndex;
+
+divs.forEach((div, index) => {
+  div.addEventListener('dragstart', () => {
+    dragStartIndex = index;
+  });
+});
+
+divs.forEach((div) => {
+  div.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+});
+
+divs.forEach((div, index) => {
+  div.addEventListener('drop', () => {
+    let temp = divs[dragStartIndex].style.backgroundImage;
+    divs[dragStartIndex].style.backgroundImage = div.style.backgroundImage;
+    div.style.backgroundImage = temp;
+  });
+});
